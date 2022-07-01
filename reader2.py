0c31ff4b3c77e9ff5f125ce8d5239542cabc7e26
Sebastian <68529971+html-proof@users.noreply.github.com>
2022-07-01 23:17:03 +0530

    Update reader2.py
    update version 1.2

--- reader2.py
+++ reader2.py
@@ -1,8 +1,10 @@
 import PyPDF2
 from tkinter import *
+from threading import *
 from tkinter import filedialog
 import pyttsx3
 import time
+
 win = Tk()
 
 win.geometry("1500x1500")
@@ -11,34 +13,38 @@ txt1 = IntVar()
 text = Text(win, width=80, height=30)
 text.pack(pady=20)
 
-try:
-    def open_pdf():
-        file = filedialog.askopenfilename(title="Select a PDF", filetype=(("PDF    Files", "*.pdf"), ("All Files", "*.*")))
-        if file:
-            pdf_file = PyPDF2.PdfFileReader(file)
-
-            page = pdf_file.getPage(txt1.get())
-
-            content = page.extractText()
-            txt = pyttsx3.init()
-            txt.say(content)
-            txt.runAndWait()
-            text.insert(1.0, content)
-            time.sleep(2)
+while True:
+    try:
+        def threading():
+            t1 = Thread(target=open_pdf)
+            t1.start()
 
 
+        def open_pdf():
+            file = filedialog.askopenfilename(title="Select a PDF")
+            if file:
+                pdf_file = PyPDF2.PdfFileReader(file)
+
+                page = pdf_file.getPage(txt1.get())
+
+                content = page.extractText()
+                txt = pyttsx3.init()
+                txt.say(content)
+                txt.runAndWait()
+                text.insert(1.0, content)
+                time.sleep(2)
 
 
-except FloatingPointError:
-    pass
 
 
-bt1_open = Button(win, text="Select the file ", command=open_pdf)
-lbl = Label(win, text="input the page to speak")
-lbl2 = Entry(win, textvariable=txt1)
-lbl.pack()
-lbl2.pack()
-bt1_open.pack()
+    except FloatingPointError:
+        pass
 
+    bt1_open = Button(win, text="Select the file ", command=open_pdf)
+    lbl = Label(win, text="input the page to speak")
+    lbl2 = Entry(win, textvariable=txt1)
+    lbl.pack()
+    lbl2.pack()
+    bt1_open.pack()
 
-win.mainloop()
+    win.mainloop()
